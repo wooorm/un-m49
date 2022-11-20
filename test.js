a0b6fd6fdcac088dcaeedd1be7eabb5c3e3c91f8
Titus Wormer <tituswormer@gmail.com>
2022-11-20 09:35:13 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,32 +1,21 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {unM49} from './index.js'
 
-test('m49', function (t) {
-  t.plan(3)
+test('m49', function () {
+  assert.ok(Array.isArray(unM49), 'should be an `array`')
 
-  t.ok(Array.isArray(unM49), 'should be an `array`')
-
-  let index = -1
-  let found = false
-
-  while (++index < unM49.length) {
-    if (unM49[index].code !== '826') {
-      continue
-    }
-
-    found = true
-    t.deepEqual(
-      unM49[index],
-      {
-        type: 4,
-        name: 'United Kingdom of Great Britain and Northern Ireland',
-        code: '826',
-        iso3166: 'GBR',
-        parent: '154'
-      },
-      'should work'
-    )
-  }
-
-  t.equal(found, true, 'expected `826` in array')
+  const gbr = unM49.find((d) => d.code === '826')
+  assert(gbr)
+  assert.deepEqual(
+    gbr,
+    {
+      type: 4,
+      name: 'United Kingdom of Great Britain and Northern Ireland',
+      code: '826',
+      iso3166: 'GBR',
+      parent: '154'
+    },
+    'should work'
+  )
 })
